0d2231341db36fe6b956ab3fceb10ed59ca779ff
pleimos <pleimos@gmail.com>
2012-12-16 17:09:33 +0900

    프로젝트 추가

---
+++ SimpleNote/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  SimpleNote
+//
+//  Created by jinwoo choi on 12/16/12.
+//  Copyright (c) 2012 pleimos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
